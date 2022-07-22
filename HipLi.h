2d57fc024c4c58c1effc2a0f49604572ca13b020
Astolfen <daniil.zarubin.03@mail.ru>
2022-07-22 19:58:20 +0300

    isHeap

---
+++ HipLi.h
@@ -0,0 +1,8 @@
+#ifndef INC_HIPLI_H
+#define INC_HIPLI_H
+
+#include <iostream>
+
+bool isHeap(int n);
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-#include "A.h"
+#include "HipLi.h"
 
 int main() {
+    int n;
+    std::cin >> n;
+
+    std::cout << isHeap(n);
 
     return 0;
 }
